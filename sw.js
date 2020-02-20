8bc86fe766c91152296821c0cdc3f9673d382792
Andrey Miskov <amiskov@gmail.com>
2020-02-20 20:33:17 +0300

    fix sw

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
-      caches.open('video-store').then(function(cache) {
-        return cache.addAll([
-          '/index.html',
-          '/static/timer.js',
-          '/static/style.css',
-          '/static/img/arny_thumbs_up.jpg',
-          '/static/sounds/bell.mp3',
-          '/static/sounds/alert.mp3',
-        ]);
-      })
+        caches.open('video-store').then(function (cache) {
+            return cache.addAll([
+                './index.html',
+                './static/timer.js',
+                './static/style.css',
+                './static/img/arny_thumbs_up.jpg',
+                './static/sounds/bell.mp3',
+                './static/sounds/alert.mp3',
+            ]);
+        })
     );
-   });
-   
-   self.addEventListener('fetch', function(e) {
-     console.log(e.request.url);
-     e.respondWith(
-       caches.match(e.request).then(function(response) {
-         return response || fetch(e.request);
-       })
-     );
-   });
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
